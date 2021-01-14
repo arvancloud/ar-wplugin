890fc8646eee2bd8cd8f131a31a35fae214da201
Amirhossein Meydani <amirhwsin@outlook.com>
2021-01-14 20:13:44 +0330

    Fixed some bugs

--- ar-wplugin.php
+++ ar-wplugin.php
@@ -9,6 +9,8 @@ Author URI: https://github.com/arvancloud/ar-wplugin/graphs/contributors
 License: GPLv2 or later
 Text Domain: ar-wplugin
 */
+if (!is_admin() || !defined( 'ABSPATH' ))
+    return;
 
 define('AR_TEXT_DOMAIN', 'ar-wplugin');
 
